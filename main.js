b6d12f8551becf3e478a25c4e7d71187f2652203
Noe Pfister¨ <noe.pfister@gmx.ch>
2025-05-05 13:44:46 +0200

    added basic buttons

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+
+
 const iron_button = document.getElementById("button-iron");
 const wood_button = document.getElementById("button-wood");
 const leather_button = document.getElementById("button-leather");
@@ -8,4 +10,32 @@ const wood_resource = document.getElementById("resource-wood");
 const leather_resource = document.getElementById("resource-leather");
 
 
-iron_button.innerHTML = "1";
+
+
+var iron_count = 1;
+var wood_count = 1;
+var leather_count = 1;
+
+update_text();
+
+iron_button.onclick = function () {
+    iron_count += 1;
+    update_text();
+};
+
+wood_button.onclick = function () {
+    wood_count += 1;
+    update_text();
+};
+
+leather_button.onclick = function () {
+    leather_count += 1;
+    update_text();
+};
+
+
+function update_text() {
+    iron_resource.innerHTML = iron_count;
+    wood_resource.innerHTML = wood_count;
+    leather_resource.innerHTML = leather_count;
+}
