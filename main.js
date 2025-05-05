3060eee5a7a70a6c452f7f9c4cc65edcadae28bd
Noe Pfister¨ <noe.pfister@gmx.ch>
2025-05-05 13:01:44 +0200

    refactored html

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const iron_button = document.getElementById("button-iron");
+const wood_button = document.getElementById("button-wood");
+const leather_button = document.getElementById("button-leather");
+
+const iron_resource = document.getElementById("resource-iron");
+const wood_resource = document.getElementById("resource-wood");
+const leather_resource = document.getElementById("resource-leather");
+
+
+iron_button.innerHTML = "1";
